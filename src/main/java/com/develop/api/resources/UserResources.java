8d44db6b52c35c780ef08688adf0b68c68c3564f
ReichertDevelop <reichert.develop@gmail.com>
2022-11-05 11:56:09 -0300

    Class UserSource

--- src/main/java/com/develop/api/resources/UserResources.java
+++ src/main/java/com/develop/api/resources/UserResources.java
@@ -1,8 +1,6 @@
 package com.develop.api.resources;
 
 import com.develop.api.domain.User;
-import com.develop.api.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,11 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/user")
 public class UserResources {
 
-    @Autowired
-    private UserService service;
-
     @GetMapping(value = "/{id}")
     public ResponseEntity<User> findById(@PathVariable Integer id) {
-        return ResponseEntity.ok().body(service.findById(id));
+        return ResponseEntity.ok().body(new User(1, "Luiz", "develop@gmail.com", "123"));
     }
 }
